47a5cab76f70aeb0aae4af753ad8e3982f772eb9
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-30 15:37:19 +0200

    Testing utilities and helpers

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,5 +1,18 @@
+import { describe, expect } from "vitest";
+import { range } from "./utils";
+
 describe("utils", () => {
-  it("should do something", () => {
-    expect(true).toBe(true);
+  describe("simple always passing test", () => {
+    it("return true", () => {
+      expect(true).toBe(true);
+    });
+  });
+
+  describe("range", () => {
+    it("return correct range from 1 to 5", () => {
+      const result = range(1, 5);
+      expect(result).toEqual([1, 2, 3, 4]);
+      expect(result).toHaveLength(4);
+    });
   });
 });
