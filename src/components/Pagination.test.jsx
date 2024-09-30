25cdd7a0ac62ac0c8813744c33ebf702f029f367
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-30 17:59:51 +0200

    Testing inputs and outputs

---
+++ src/components/Pagination.test.jsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Pagination from "./Pagination";
+import { it } from "vitest";
+
+describe("Pagination", () => {
+  ///
+  it("should render component correctly", () => {
+    render(<Pagination total={50} limit={10} currentPage={1} />);
+
+    const listItems = screen.getAllByTestId("page-container");
+    expect(listItems).toHaveLength(5);
+
+    listItems.forEach((item) => {
+      expect(item).toHaveTextContent(item.textContent);
+    });
+  });
+  ////
+
+  it("should select page correctly", () => {
+    const selectPageMockFn = vi.fn();
+    render(
+      <Pagination
+        total={50}
+        limit={10}
+        currentPage={1}
+        selectPage={selectPageMockFn}
+      />
+    );
+    screen.debug();
+
+    const listItems = screen.getAllByTestId("page-container");
+    const user = userEvent.setup();
+    listItems.forEach(async (item) => {
+      await user.click();
+      expect(selectPage).toHaveBeenCalledWith(item.textContent);
+    });
+  });
+});
