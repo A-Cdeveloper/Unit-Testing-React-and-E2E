a231edaa02a5d5ebc97580ce3d21e12b13d7e92d
Aleksandar <aleksandar@e-vlasotince.info>
2024-10-01 02:12:24 +0200

    vi.spyOn axios mockResolvedValue

--- src/components/Tags.test.jsx
+++ src/components/Tags.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Tags from "./Tags";
+import axios from "axios";
 
 /////////
 
@@ -11,5 +12,14 @@ describe("Tags", () => {
     tags.forEach((tag, index) => {
       expect(tag).toHaveTextContent(`tag${index + 1}`);
     });
+  });
+
+  it("should render tag axios spy", async () => {
+    vi.spyOn(axios, "get").mockResolvedValue({
+      data: [{ id: 1, name: "tag1" }],
+    });
+    render(<Tags />);
+    const tags = await screen.findAllByTestId("tag");
+    expect(tags).toHaveLength(1);
   });
 });
